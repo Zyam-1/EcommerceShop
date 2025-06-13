783cbd27f3dd462468329e3079e07a57d2d2ddba
Zyam-1 <zyammaqsood7@gmail.com>
2025-06-13 15:35:20 +0500

    Working on ProductPage

--- frontend/src/screens/ProductPage.jsx
+++ frontend/src/screens/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Row, Col, ListGroup, ListGroupItem, Button, Image } from "react-bootstrap";
 import { Link, useParams, useNavigate } from 'react-router-dom';
+import Rating from "../components/Rating";
 import products from "../products";
 
 
@@ -23,13 +24,30 @@ const ProductPage = () => {
                 </Col>
                 <Col md={3}>
                     <ListGroup>
-                        <ListGroupItem>{product.name}</ListGroupItem>
-                    </ListGroup>
-                    <ListGroup>
+                        <ListGroupItem><h2>{product.name}</h2></ListGroupItem>
                         <ListGroupItem>{product.description}</ListGroupItem>
+                        <ListGroupItem>
+                            <Rating ratings={product.rating} reviews={product.numReviews} />
+                        </ListGroupItem>
+
+                        <ListGroupItem><h3>${product.price}</h3></ListGroupItem>
                     </ListGroup>
                 </Col>
                 <Col md={3}>
+                    <ListGroup>
+                        <ListGroupItem>
+                            <Row>
+                                <Col>
+                                    Status:
+                                </Col>
+                                <Col>
+                                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                                </Col>
+                            </Row>
+                        </ListGroupItem>
+                        <ListGroupItem><Button varient="button" disabled={product.countInStock === 0}>Add to Cart</Button></ListGroupItem>
+
+                    </ListGroup>
                 </Col>
             </Row>
         </>
